54c8917954ff24c809880e8d8e99d86d289d7519
alfre <alfredo.medina@uc.cl>
2023-11-26 14:49:10 -0300

    prettier

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,68 +1,68 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 // import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-
+import { useContext } from "react";
 
 const invisibleStyle = {
-    display: "none",
-}
+  display: "none",
+};
 const visibleStyle = {
-    display: "block",
+  display: "block",
+};
+
+function Rules(props) {
+  const context = useContext(props.context);
+
+  return (
+    <Container>
+      <Title>
+        Reroute the knight to the <Highlighted>highlighted square</Highlighted>
+      </Title>
+
+      <span style={context.exercise < 3 ? visibleStyle : invisibleStyle}>
+        The goal is to move the knight in order
+        <OrderRoute ascending={context.exercise !== 1} />, with the restriction
+        that you can't move it to controlled squares by the black pieces.
+      </span>
+
+      <span style={context.exercise >= 3 ? visibleStyle : invisibleStyle}>
+        Go check that king with your horsy!
+      </span>
+    </Container>
+  );
 }
 
-function Rules(props){
-    const context = useContext(props.context);
-    
-    return (
-        <Container>
-            <Title>Reroute the knight to the <Highlighted>highlighted square</Highlighted></Title>
-            
-            <span style={context.exercise<3? visibleStyle: invisibleStyle}>
-                The goal is to move the knight in order 
-                <OrderRoute ascending={context.exercise!==1}/>
-                , with the restriction 
-                that you can't move it to controlled squares by the black pieces.
-            </span>
-
-            <span style={context.exercise>=3? visibleStyle: invisibleStyle}>
-                Go check that king with your horsy!
-            </span>
-        </Container>
-    )
-}
-
-function OrderRoute(props){
-    const ascending = props.ascending;
-    return (
-        <span>
-            {ascending? " left to right, bottom to up": " right to left, up to bottom"}
-        </span>
-    )
+function OrderRoute(props) {
+  const ascending = props.ascending;
+  return (
+    <span>
+      {ascending
+        ? " left to right, bottom to up"
+        : " right to left, up to bottom"}
+    </span>
+  );
 }
 
 export default Rules;
 
-
-
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 2%;
-    max-width: 300px;
-    margin-bottom: 40px;
-    @media (max-width: 900px) { 
-        order: 5;
-      }
+  display: flex;
+  flex-direction: column;
+  margin: 2%;
+  max-width: 300px;
+  margin-bottom: 40px;
+  @media (max-width: 900px) {
+    order: 5;
+  }
 `;
 
 const Title = styled.h3`
-    text-align: center;
+  text-align: center;
 `;
 
 const Highlighted = styled.span`
-    color: #4183c4;
-    text-decoration: none;
-    font-weight: bold;
-    display: inline-block;
-    width: fit-content;
+  color: #4183c4;
+  text-decoration: none;
+  font-weight: bold;
+  display: inline-block;
+  width: fit-content;
 `;
